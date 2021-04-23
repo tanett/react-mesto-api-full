c4ec55e66a920e206846e34ee9c55df8af0f084c
Maxim <samai2@yandex.ru>
2021-04-23 14:39:35 +0300

    аторизация на куках, возврат объекта дата 1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,15 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  if (!token) {
     return res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
-
-  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
